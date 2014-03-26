624d9164b3687356aee27dc64c9654ccd2a6de0d
Marlon Richert <marlon@vaadin.com>
2014-03-26 15:16:38 +0200

    make TSearchField public

--- tribestream-registry/src/main/java/com/tomitribe/components/TSearchField.java
+++ tribestream-registry/src/main/java/com/tomitribe/components/TSearchField.java
@@ -2,7 +2,7 @@ package com.tomitribe.components;
 
 import com.vaadin.ui.TextField;
 
-class TSearchField extends TextField {
+public class TSearchField extends TextField {
     public TSearchField(String inputPrompt) {
         addStyleName(TribestreamTheme.SEARCH);
         setInputPrompt(inputPrompt);
