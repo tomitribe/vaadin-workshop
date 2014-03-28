b369d852948b0896888834538ec17c7c11afdc28
Marlon Richert <marlon@vaadin.com>
2014-03-28 13:43:18 +0200

    fix breadcrumb trail

--- tribestream-registry/src/main/java/com/tomitribe/tribestream/registry/components/TBreadcrumbTrail.java
+++ tribestream-registry/src/main/java/com/tomitribe/tribestream/registry/components/TBreadcrumbTrail.java
@@ -6,25 +6,24 @@ import com.vaadin.navigator.Navigator;
 public class TBreadcrumbTrail extends THorizontalLayout {
     public TBreadcrumbTrail(Navigator navigator) {
         addStyleName(TribestreamTheme.StyleNames.BREADCRUMB_TRAIL);
+
         String state = navigator.getState();
         if (state.isEmpty()) {
             addComponent(new TLabel("Home"));
         } else {
-            String path = navigator.getUI().getPage().getLocation().getPath();
+            String path = navigator.getUI().getPage().getLocation().getPath() + "#!";
             addComponent(new TBreadcrumb("Home", path));
-            String[] tokens = state.split("/");
-            path += "#!";
-            TBreadcrumb last = null;
-            for (String token : tokens) {
-                if (token.length() > 0) {
-                    path += token;
-                    addComponent(last = new TBreadcrumb(token, path));
-                    path += '/';
-                }
-            }
-            if (last != null) {
-                removeComponent(last);
-                addComponent(new TLabel(last.getCaption()));
+
+            int slash = state.indexOf("/");
+
+            if (slash == -1) {
+                addComponent(new TLabel(state));
+            } else {
+                String repo = state.substring(0, slash);
+                String resource = state.substring(slash);
+
+                addComponent(new TBreadcrumb(repo, path + repo));
+                addComponent(new TLabel(resource));
             }
         }
     }
